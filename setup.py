113f516f5fc4059d9bcdcc0dacc5f67900fab19e
Ashish Soni <ashishsoni295work@gmail.com>
2025-12-19 00:06:46 +0530

    First run of model successful

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A clean, organized project that uses machine learning to identify the emotional tone behind written language.',
+    author='Ashish Soni',
+)
